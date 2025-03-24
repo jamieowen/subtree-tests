5f06884fd39405d0b509713f5066118215b589d7
ethanchiu10 <ethan@ethanchiu.dev>
2025-03-24 11:41:21 +0700

    linera light

--- src/webgl/material-module/modules/MaterialModuleSpriteAnimated.jsx
+++ src/webgl/material-module/modules/MaterialModuleSpriteAnimated.jsx
@@ -60,6 +60,46 @@ export const MaterialModuleSpriteAnimated = forwardRef(
             return (blendHardLight(base, blend) * opacity + base * (1.0 - opacity));
           }
 
+          float blendLinearBurn(float base, float blend) {
+            // Note : Same implementation as BlendSubtractf
+            return max(base+blend-1.0,0.0);
+          }
+          
+          vec3 blendLinearBurn(vec3 base, vec3 blend) {
+            // Note : Same implementation as BlendSubtract
+            return max(base+blend-vec3(1.0),vec3(0.0));
+          }
+          
+          vec3 blendLinearBurn(vec3 base, vec3 blend, float opacity) {
+            return (blendLinearBurn(base, blend) * opacity + base * (1.0 - opacity));
+          }
+
+          float blendLinearDodge(float base, float blend) {
+            // Note : Same implementation as BlendAddf
+            return min(base+blend,1.0);
+          }
+          
+          vec3 blendLinearDodge(vec3 base, vec3 blend) {
+            // Note : Same implementation as BlendAdd
+            return min(base+blend,vec3(1.0));
+          }
+          
+          vec3 blendLinearDodge(vec3 base, vec3 blend, float opacity) {
+            return (blendLinearDodge(base, blend) * opacity + base * (1.0 - opacity));
+          }
+
+          float blendLinearLight(float base, float blend) {
+            return blend<0.5?blendLinearBurn(base,(2.0*blend)):blendLinearDodge(base,(2.0*(blend-0.5)));
+          }
+          
+          vec3 blendLinearLight(vec3 base, vec3 blend) {
+            return vec3(blendLinearLight(base.r,blend.r),blendLinearLight(base.g,blend.g),blendLinearLight(base.b,blend.b));
+          }
+          
+          vec3 blendLinearLight(vec3 base, vec3 blend, float opacity) {
+            return (blendLinearLight(base, blend) * opacity + base * (1.0 - opacity));
+          }
+
         `,
         main: /*glsl*/ `
           float frameNum = uSprite_Frame;
@@ -72,9 +112,9 @@ export const MaterialModuleSpriteAnimated = forwardRef(
           );
 
           pc_fragColor.a = frameColor.a;
-          pc_fragColor.rgb = blendHardLight(vec3(1.0, 0.0, 0.0), frameColor.rgb);
+          // pc_fragColor.rgb = blendHardLight(vec3(1.0, 0.0, 0.0), frameColor.rgb);
+          pc_fragColor.rgb = blendLinearLight(vec3(1.0, 0.0, 0.0), frameColor.rgb);
 
-          // pc_fragColor = frameColor;
         `,
       },
     });
