1927eee7c97387952c6d11899db83d8fd9e2c1b0
Ethan Chiu <ethan@ethanchiu.dev>
2025-04-01 23:47:39 +0700

    ending

--- src/components/pages/ending/Ending.jsx
+++ src/components/pages/ending/Ending.jsx
@@ -6,8 +6,13 @@ export const Ending = ({ show, ...props }) => {
   const { t } = useTranslation();
   const setPage = useAppStore((state) => state.setPage);
   return (
-    <div className={classnames(['page', 'Ending', { show }])}>
-      <h1>Ending</h1>
+    <div className={classnames(['page', 'ending', { show }])}>
+      <div className="wrap">
+        <div className="preheading">{t('ending.preheading')}</div>
+        <h1>{t('ending.heading')}</h1>
+        <button className="btn-secondary">{t('ending.restart')}</button>
+      </div>
+      <ButtonPrimary>{t('ending.cta')}</ButtonPrimary>
     </div>
   );
 };
