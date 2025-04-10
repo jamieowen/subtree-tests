f9a41220c834ae7b05a125e6ad01755139a37fb0
Ethan Chiu <ethan@ethanchiu.dev>
2025-04-10 23:02:17 +0700

    delay countdown

--- src/components/_misc/game/Countdown.jsx
+++ src/components/_misc/game/Countdown.jsx
@@ -2,7 +2,7 @@ import './Countdown.sass';
 import classnames from 'classnames';
 import AssetService from '@/services/AssetService';
 import { gsap } from 'gsap';
-
+import { PromiseTimeout } from '@/helpers/PromiseTimeout';
 export const Countdown = ({
   id,
   duration = 3,
@@ -19,24 +19,28 @@ export const Countdown = ({
   useEffect(() => {
     if (!show) return;
 
-    setTime(duration);
-    animateNum();
-    AssetService.getAsset('sfx_countdown').play();
+    let interval;
+    (async () => {
+      await PromiseTimeout(600);
+      setTime(duration);
+      animateNum();
+      AssetService.getAsset('sfx_countdown').play();
 
-    let interval = setInterval(() => {
-      setTime((time) => {
-        if (time === 1) {
-          AssetService.getAsset('sfx_start').play();
-          clearInterval(interval);
-          onEnded();
-          return 1;
-        } else {
-          AssetService.getAsset('sfx_countdown').play();
-          animateNum();
-          return time - 1;
-        }
-      });
-    }, 1000);
+      interval = setInterval(() => {
+        setTime((time) => {
+          if (time === 1) {
+            AssetService.getAsset('sfx_start').play();
+            clearInterval(interval);
+            onEnded();
+            return 1;
+          } else {
+            AssetService.getAsset('sfx_countdown').play();
+            animateNum();
+            return time - 1;
+          }
+        });
+      }, 1000);
+    })();
 
     return () => clearInterval(interval);
   }, [show]);
@@ -48,9 +52,13 @@ export const Countdown = ({
     if (tween.current) tween.current.kill();
     let tl = gsap.timeline();
     tween.current = tl;
-    tl.add('start');
+
+    tl.add('reset');
     tl.set(refNum.current, { opacity: 1, scale: 1.1 });
     tl.set('.countdown__inner', { scale: 1.2 });
+
+    tl.add('start', 'reset');
+
     tl.to(refNum.current, { scale: 0.9, ease: 'none', duration: 1 }, 'start');
 
     tl.add('inner', 'start');
@@ -81,7 +89,7 @@ export const Countdown = ({
           className="preheading type-body1"
           initial={{ opacity: 0 }}
           animate={{ opacity: show ? 1 : 0 }}
-          transition={{ duration: 0.3 }}
+          transition={{ duration: 0.3, delay: 1 }}
         >
           {t(`${id}.game.ready`)}
         </motion.div>
